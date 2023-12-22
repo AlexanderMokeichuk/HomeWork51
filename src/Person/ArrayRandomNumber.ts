a9dcdae23bde8633b76e04e997718f22cceb4ad0
Alexander Mokei <alexander.q.qa.a@mail.ru>
2023-12-22 16:42:23 +0600

    Created a function to generate numbers

---
+++ src/Person/ArrayRandomNumber.ts
@@ -0,0 +1,15 @@
+const random = () => {
+    const result = [];
+
+    while (result.length < 5) {
+        const randomNumber = Math.floor(Math.random() * (36 - 5)) + 5;
+        if (result.indexOf(randomNumber) === -1) {
+            result.push(randomNumber);
+        }
+    }
+
+    return (
+        result
+    )
+}
+export default random;
