31f2abdfe8b865f3097fdf54b98d74609c6c67e0
Alexander Mokei <alexander.q.qa.a@mail.ru>
2023-12-22 16:47:03 +0600

    Removed unnecessary files, edited the code

--- src/Person/ArrayRandomNumber.ts
+++ src/Person/ArrayRandomNumber.ts
@@ -1,15 +1,16 @@
 const random = () => {
-    const result = [];
+  const result = [];
 
-    while (result.length < 5) {
-        const randomNumber = Math.floor(Math.random() * (36 - 5)) + 5;
-        if (result.indexOf(randomNumber) === -1) {
-            result.push(randomNumber);
-        }
+  while (result.length < 5) {
+    const randomNumber = Math.floor(Math.random() * (36 - 5)) + 5;
+    if (result.indexOf(randomNumber) === -1) {
+      result.push(randomNumber);
     }
+  }
 
-    return (
-        result
-    )
-}
+  return (
+    result
+  );
+};
+
 export default random;
--- src/main.tsx
+++ src/main.tsx
@@ -1,50 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import RandomNumber from './Person/ArrayRandomNumber.ts'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client"
+import RandomNumber from "./Person/ArrayRandomNumber.ts";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 let arrayNumber = [5, 11, 16, 23, 33];
 
 const buttonEvent = () => {
-    arrayNumber = RandomNumber();
-    arrayNumber = arrayNumber.sort((n1,n2) => n1 - n2);
-    root.render(
-        <React.StrictMode>
-            <div>
-                <div>
-                    <button onClick={buttonEvent} className="buttonEvent">New numbers</button>
-                </div>
-                <div className="balls">
-                    <div className="ball"> <h6 className="number">{arrayNumber[0]}</h6> </div>
-                    <div className="ball"> <h6 className="number">{arrayNumber[1]}</h6> </div>
-                    <div className="ball"> <h6 className="number">{arrayNumber[2]}</h6> </div>
-                    <div className="ball"> <h6 className="number">{arrayNumber[3]}</h6> </div>
-                    <div className="ball"> <h6 className="number">{arrayNumber[4]}</h6> </div>
-                </div>
-            </div>
-        </React.StrictMode>,
-    )
-}
-
-const container = (
-    <div>
+  arrayNumber = RandomNumber();
+  arrayNumber = arrayNumber.sort((n1, n2) => n1 - n2);
+  root.render(
+    <React.StrictMode>
+      <div>
         <div>
-            <button onClick={buttonEvent} className="buttonEvent">New numbers</button>
+          <button onClick={buttonEvent} className="buttonEvent">New numbers</button>
         </div>
         <div className="balls">
-            <div className="ball"> <h6 className="number">{arrayNumber[0]}</h6> </div>
-            <div className="ball"> <h6 className="number">{arrayNumber[1]}</h6> </div>
-            <div className="ball"> <h6 className="number">{arrayNumber[2]}</h6> </div>
-            <div className="ball"> <h6 className="number">{arrayNumber[3]}</h6> </div>
-            <div className="ball">< h6 className="number">{arrayNumber[4]}</h6> </div>
+          <div className="ball"><h6 className="number">{arrayNumber[0]}</h6></div>
+          <div className="ball"><h6 className="number">{arrayNumber[1]}</h6></div>
+          <div className="ball"><h6 className="number">{arrayNumber[2]}</h6></div>
+          <div className="ball"><h6 className="number">{arrayNumber[3]}</h6></div>
+          <div className="ball"><h6 className="number">{arrayNumber[4]}</h6></div>
         </div>
+      </div>
+    </React.StrictMode>,
+  );
+};
+
+const container = (
+  <div>
+    <div>
+      <button onClick={buttonEvent} className="buttonEvent">New numbers</button>
     </div>
-)
+    <div className="balls">
+      <div className="ball"><h6 className="number">{arrayNumber[0]}</h6></div>
+      <div className="ball"><h6 className="number">{arrayNumber[1]}</h6></div>
+      <div className="ball"><h6 className="number">{arrayNumber[2]}</h6></div>
+      <div className="ball"><h6 className="number">{arrayNumber[3]}</h6></div>
+      <div className="ball">< h6 className="number">{arrayNumber[4]}</h6></div>
+    </div>
+  </div>
+);
 
 root.render(
-    <React.StrictMode>
-        {container}
-    </React.StrictMode>,
-)
+  <React.StrictMode>
+    {container}
+  </React.StrictMode>,
+);
 
